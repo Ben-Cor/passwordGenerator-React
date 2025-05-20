2fd0f5192107f3a099c4ca244892f13c73f46bdf
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-05-20 15:41:45 +0100

    spacing adjusted

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="bg-gray-800 text-white p-4 flex justify-center items-center">
+    <header className="bg-gray-800 text-white p-8 flex justify-center items-center">
       <h1 className="font-tektur text-3xl font-bold">Password Generator</h1>
     </header>
   );
